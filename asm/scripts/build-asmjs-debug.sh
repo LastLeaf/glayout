68b8bb0ae617cfb372833e0237ff4306a419d2fd
LastLeaf <bqfu@163.com>
2018-06-04 00:01:59 +0800

    Impl

--- asm/scripts/build-asmjs-debug.sh
+++ asm/scripts/build-asmjs-debug.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-EMMAKEN_CFLAGS="-s EXTRA_EXPORTED_RUNTIME_METHODS=[\'UTF8ToString\'] --js-library ../lib/bin/interfaces-debug.js --pre-js scripts/pre.js --post-js scripts/post.js" cargo build --target=asmjs-unknown-emscripten
+EMMAKEN_CFLAGS="-s WASM=0 -s SINGLE_FILE=1 -s EXTRA_EXPORTED_RUNTIME_METHODS=[\'UTF8ToString\'] --js-library ../lib/bin/interfaces-debug.js --pre-js scripts/pre.js --post-js scripts/post.js" cargo build --target=asmjs-unknown-emscripten
--- asm/scripts/build-asmjs-release.sh
+++ asm/scripts/build-asmjs-release.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-EMMAKEN_CFLAGS="--js-library ../lib/bin/interfaces-release.js --pre-js scripts/pre.js --post-js scripts/post.js --llvm-lto 3 -O3 -Os --closure 1" cargo build --target=asmjs-unknown-emscripten --release
+EMMAKEN_CFLAGS="-s WASM=0 -s SINGLE_FILE=1 --js-library ../lib/bin/interfaces-release.js --pre-js scripts/pre.js --post-js scripts/post.js --llvm-lto 3 -O3 -Os --closure 1" cargo build --target=asmjs-unknown-emscripten --release
